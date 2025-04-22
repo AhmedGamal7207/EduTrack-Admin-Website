af2bb962a65e3d039b1814f0df68c2b8b943b142
AhmedGamal7207 <92176653+AhmedGamal7207@users.noreply.github.com>
2025-04-22 05:40:40 +0200

    Added Lottie Loading
    Co-Authored-By: Myar Hany <206391813+MyarHany@users.noreply.github.com>

--- edutrack_admin_web/lib/screens/dashboard_screens/dashboard_screen.dart
+++ edutrack_admin_web/lib/screens/dashboard_screens/dashboard_screen.dart
@@ -11,115 +11,150 @@ import 'package:edutrack_admin_web/widgets/headers/header_widget.dart';
 import 'package:edutrack_admin_web/widgets/graphs_and_tables/line_chart_widget.dart';
 import 'package:edutrack_admin_web/widgets/cards/stats_card_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
 
   @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    simulateLoading();
+  }
+
+  void simulateLoading() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    // Simulate a 2-second loading period
+    await Future.delayed(const Duration(seconds: 4));
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(Constants.pagePadding),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            HeaderWidget(headerTitle: "Dashboard"),
-            SizedBox(height: Constants.internalSpacing),
-            Row(
-              mainAxisSize: MainAxisSize.max,
+    return isLoading
+        ? Center(
+          child: Lottie.asset(
+            'assets/lotties/student_loading.json',
+            width: 450,
+            height: 450,
+          ),
+        )
+        : Padding(
+          padding: const EdgeInsets.all(Constants.pagePadding),
+          child: SingleChildScrollView(
+            child: Column(
               children: [
-                Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children:
-                        StatsData().statsData
-                            .map(
-                              (stat) => Expanded(
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 8,
+                HeaderWidget(headerTitle: "Dashboard"),
+                SizedBox(height: Constants.internalSpacing),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children:
+                            StatsData().statsData
+                                .map(
+                                  (stat) => Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                        horizontal: 8,
+                                      ),
+                                      child: StatCardWidget(model: stat),
+                                    ),
                                   ),
-                                  child: StatCardWidget(model: stat),
+                                )
+                                .toList(),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: Constants.internalSpacing),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Expanded(
+                      child: WhiteContainer(
+                        child: LineChartCard(
+                          graphTitle: "Students Attendance",
+                          data: AttendanceLineData(),
+                          showDateRow: true,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: Constants.internalSpacing),
+                    Expanded(
+                      child: WhiteContainer(
+                        child: LineChartCard(
+                          graphTitle: "Students Concentration",
+                          data: ConcentrationLineData(),
+                          showDateRow: true,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: Constants.internalSpacing),
+                WhiteContainer(
+                  child: FlexibleSmartTable<AttendanceWarningsTableModel>(
+                    title: "Attendance Warnings",
+                    columnNames: [
+                      "Name",
+                      "ID",
+                      "Grade",
+                      "Class",
+                      "Number of Absences",
+                    ],
+                    data: AttendanceWarningsData.warnings,
+                    onRowTap: (row) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder:
+                              (context) => HomeScreen(
+                                subScreen: StudentScreen(
+                                  name: row.name,
+                                  id: row.id,
                                 ),
+                                selectedIndex: 2,
                               ),
-                            )
-                            .toList(),
+                        ),
+                      );
+                    },
+                    getValue: (row, column) {
+                      switch (column) {
+                        case "Name":
+                          return row.name;
+                        case "ID":
+                          return row.id;
+                        case "Grade":
+                          return row.grade;
+                        case "Class":
+                          return row.studentClass;
+                        case "Number of Absences":
+                          return row.absences.toString();
+                        default:
+                          return "";
+                      }
+                    },
                   ),
                 ),
               ],
             ),
-            SizedBox(height: Constants.internalSpacing),
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Expanded(
-                  child: WhiteContainer(
-                    child: LineChartCard(
-                      graphTitle: "Students Attendance",
-                      data: AttendanceLineData(),
-                      showDateRow: true,
-                    ),
-                  ),
-                ),
-                SizedBox(width: Constants.internalSpacing),
-                Expanded(
-                  child: WhiteContainer(
-                    child: LineChartCard(
-                      graphTitle: "Students Concentration",
-                      data: ConcentrationLineData(),
-                      showDateRow: true,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: Constants.internalSpacing),
-            WhiteContainer(
-              child: FlexibleSmartTable<AttendanceWarningsTableModel>(
-                title: "Attendance Warnings",
-                columnNames: [
-                  "Name",
-                  "ID",
-                  "Grade",
-                  "Class",
-                  "Number of Absences",
-                ],
-                data: AttendanceWarningsData.warnings,
-                onRowTap: (row) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder:
-                          (context) => HomeScreen(
-                            subScreen: StudentScreen(
-                              name: row.name,
-                              id: row.id,
-                            ),
-                            selectedIndex: 2,
-                          ),
-                    ),
-                  );
-                },
-                getValue: (row, column) {
-                  switch (column) {
-                    case "Name":
-                      return row.name;
-                    case "ID":
-                      return row.id;
-                    case "Grade":
-                      return row.grade;
-                    case "Class":
-                      return row.studentClass;
-                    case "Number of Absences":
-                      return row.absences.toString();
-                    default:
-                      return "";
-                  }
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        );
   }
 }
--- edutrack_admin_web/lib/screens/schedule_screens/grade_schedule_page.dart
+++ edutrack_admin_web/lib/screens/schedule_screens/grade_schedule_page.dart
@@ -3,6 +3,7 @@ import 'package:edutrack_admin_web/constants/constants.dart';
 import 'package:edutrack_admin_web/widgets/buttons/custom_button_widget.dart';
 import 'package:edutrack_admin_web/widgets/headers/header_widget.dart';
 import 'package:edutrack_admin_web/widgets/white_container_widget.dart';
+import 'package:lottie/lottie.dart';
 
 class GradeSchedulePage extends StatefulWidget {
   final String gradeNumber;
@@ -20,10 +21,11 @@ class _GradeSchedulePageState extends State<GradeSchedulePage> {
 
   late List<List<Map<String, String>>> scheduleData;
   bool _isSaved = false;
-
+  bool isLoading = true;
   @override
   void initState() {
     super.initState();
+    simulateLoading();
     scheduleData = List.generate(
       7, // 7 rows for periods
       (_) => List.generate(
@@ -33,124 +35,152 @@ class _GradeSchedulePageState extends State<GradeSchedulePage> {
     );
   }
 
+  void simulateLoading() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    // Simulate a 2-second loading period
+    await Future.delayed(const Duration(seconds: 2));
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(Constants.pagePadding),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            HeaderWidget(
-              headerTitle:
-                  'Grade ${widget.gradeNumber} Schedule${_isSaved ? " - Saved" : ""}',
-            ),
-            const SizedBox(height: Constants.internalSpacing),
-            Expanded(
-              child: WhiteContainer(
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      Table(
-                        border: TableBorder.all(color: Colors.grey.shade300),
-                        columnWidths: const {0: FixedColumnWidth(100)},
-                        children: [
-                          // Header Row
-                          TableRow(
-                            decoration: const BoxDecoration(
-                              color: Constants.scheduleHeaderBg,
-                            ),
+      body:
+          isLoading
+              ? Center(
+                child: Lottie.asset(
+                  'assets/lotties/student_loading.json',
+                  width: 450,
+                  height: 450,
+                ),
+              )
+              : Padding(
+                padding: const EdgeInsets.all(Constants.pagePadding),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    HeaderWidget(
+                      headerTitle:
+                          'Grade ${widget.gradeNumber} Schedule${_isSaved ? " - Saved" : ""}',
+                    ),
+                    const SizedBox(height: Constants.internalSpacing),
+                    Expanded(
+                      child: WhiteContainer(
+                        child: SingleChildScrollView(
+                          child: Column(
                             children: [
-                              const SizedBox(),
-                              ...days.map(
-                                (day) => Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    vertical: 12,
-                                  ),
-                                  child: Center(
-                                    child: Text(
-                                      day,
-                                      style: Constants.subHeadingStyle,
-                                    ),
-                                  ),
+                              Table(
+                                border: TableBorder.all(
+                                  color: Colors.grey.shade300,
                                 ),
-                              ),
-                            ],
-                          ),
+                                columnWidths: const {0: FixedColumnWidth(100)},
+                                children: [
+                                  // Header Row
+                                  TableRow(
+                                    decoration: const BoxDecoration(
+                                      color: Constants.scheduleHeaderBg,
+                                    ),
+                                    children: [
+                                      const SizedBox(),
+                                      ...days.map(
+                                        (day) => Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                            vertical: 12,
+                                          ),
+                                          child: Center(
+                                            child: Text(
+                                              day,
+                                              style: Constants.subHeadingStyle,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
 
-                          // Data Rows
-                          for (int row = 0; row < scheduleData.length; row++)
-                            TableRow(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Center(
-                                    child: Text(
-                                      'S${row + 1}',
-                                      style: Constants.subHeadingStyle,
-                                    ),
-                                  ),
-                                ),
-                                ...List.generate(days.length, (col) {
-                                  return Padding(
-                                    padding: const EdgeInsets.all(6.0),
-                                    child: Column(
+                                  // Data Rows
+                                  for (
+                                    int row = 0;
+                                    row < scheduleData.length;
+                                    row++
+                                  )
+                                    TableRow(
                                       children: [
-                                        _buildDropdown(
-                                          items: subjectOptions,
-                                          hint: 'Subject',
-                                          value:
-                                              scheduleData[row][col]['subject'],
-                                          onChanged: (value) {
-                                            setState(() {
-                                              scheduleData[row][col]['subject'] =
-                                                  value!;
-                                            });
-                                          },
+                                        Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Center(
+                                            child: Text(
+                                              'S${row + 1}',
+                                              style: Constants.subHeadingStyle,
+                                            ),
+                                          ),
                                         ),
-                                        const SizedBox(height: 6),
-                                        _buildDropdown(
-                                          items: teacherOptions,
-                                          hint: 'Teacher',
-                                          value:
-                                              scheduleData[row][col]['teacher'],
-                                          onChanged: (value) {
-                                            setState(() {
-                                              scheduleData[row][col]['teacher'] =
-                                                  value!;
-                                            });
-                                          },
-                                        ),
+                                        ...List.generate(days.length, (col) {
+                                          return Padding(
+                                            padding: const EdgeInsets.all(6.0),
+                                            child: Column(
+                                              children: [
+                                                _buildDropdown(
+                                                  items: subjectOptions,
+                                                  hint: 'Subject',
+                                                  value:
+                                                      scheduleData[row][col]['subject'],
+                                                  onChanged: (value) {
+                                                    setState(() {
+                                                      scheduleData[row][col]['subject'] =
+                                                          value!;
+                                                    });
+                                                  },
+                                                ),
+                                                const SizedBox(height: 6),
+                                                _buildDropdown(
+                                                  items: teacherOptions,
+                                                  hint: 'Teacher',
+                                                  value:
+                                                      scheduleData[row][col]['teacher'],
+                                                  onChanged: (value) {
+                                                    setState(() {
+                                                      scheduleData[row][col]['teacher'] =
+                                                          value!;
+                                                    });
+                                                  },
+                                                ),
+                                              ],
+                                            ),
+                                          );
+                                        }),
                                       ],
                                     ),
-                                  );
-                                }),
-                              ],
-                            ),
-                        ],
-                      ),
-                      const SizedBox(height: 20),
-                      if (!_isSaved)
-                        Align(
-                          alignment: Alignment.center,
-                          child: CustomButton(
-                            text: "Save Schedule",
-                            onTap: () {
-                              setState(() {
-                                _isSaved = true;
-                              });
-                            },
-                            hasIcon: false,
+                                ],
+                              ),
+                              const SizedBox(height: 20),
+                              if (!_isSaved)
+                                Align(
+                                  alignment: Alignment.center,
+                                  child: CustomButton(
+                                    text: "Save Schedule",
+                                    onTap: () {
+                                      setState(() {
+                                        _isSaved = true;
+                                      });
+                                    },
+                                    hasIcon: false,
+                                  ),
+                                ),
+                            ],
                           ),
                         ),
-                    ],
-                  ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 
